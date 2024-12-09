062bde0e97aead4a5b457aed181c63da94010929
Max Fazeli <m.fazeli@student.tugraz.at>
2024-12-09 17:10:43 +0100

    feat(filtering): Cleared all obsolete fields

--- scripts/submissions/sub_7_cleaned_submissions.py
+++ scripts/submissions/sub_7_cleaned_submissions.py
@@ -6,25 +6,24 @@ db = client['reddit_data']
 source_collection = db['[6]top_500_submissions_per_domain']
 target_collection = db['[7]cleaned_submissions']
 
-# List of fields to remove
-fields_to_remove = [
-    "link_flair_css_class", "edited", "is_self", "media_embed", "domain", "thumbnail",
-    "hidden", "saved", "over_18", "gilded", "retrieved_on", "stickied",
-    "secure_media_embed", "archived", "quarantine", "hide_score", "locked"
-]
+# List of fields to retain
+fields_to_keep = {
+    "_id", "downs", "link_flair_text", "url", "id", "created_utc", "name", "subreddit",
+    "title", "author", "permalink", "selftext", "subreddit_id",
+    "ups", "score", "created", "comments"
+}
 
 # Fetch all documents from the source collection
 documents = source_collection.find()
 
-# Remove specified fields and store in a new collection
+# Retain only specified fields and store in a new collection
 cleaned_documents = []
 for doc in documents:
-    for field in fields_to_remove:
-        doc.pop(field, None)
-    cleaned_documents.append(doc)
+    cleaned_doc = {key: doc[key] for key in fields_to_keep if key in doc}
+    cleaned_documents.append(cleaned_doc)
 
 # Insert cleaned documents into the target collection
 if cleaned_documents:
     target_collection.insert_many(cleaned_documents)
 
-print("Fields removed and cleaned documents stored in a new collection successfully.")
+print("Documents cleaned and stored in a new collection successfully.")
